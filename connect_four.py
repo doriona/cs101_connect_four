5fe05cc89bcd0cda8ae4f7e55b54604bc3dde73e
doriona <82111572+doriona@users.noreply.github.com>
2021-10-14 17:10:06 +0200

    add functions to check rows and columns for four pieces

--- connect_four.py
+++ connect_four.py
@@ -23,7 +23,7 @@ def print_board(board, num_rows, num_columns):
     print(("+-" * num_columns) + "+\n" )
 
 def place_piece(board, column, piece, num_rows, num_columns):
-    if column >= num_columns:
+    if column > num_columns:
         print("Trying to place an {piece} in column {column}".format(
                 piece = piece, column = column))
         print("Make sure to pick a column between 1 and {num_columns}".format(
@@ -37,8 +37,86 @@ def place_piece(board, column, piece, num_rows, num_columns):
     print("Column {column} is full. Please pick a column that is not full".format(
         column = column))
 
+def check_board_rows(board, num_rows, num_columns):
+    # check rows
+    for row in range(1, num_rows + 1):
+        count_x = 0
+        count_o = 0
+        for column in range(1, num_columns + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                count_x = 0
+                count_y = 0
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_o += 1
+                count_x = 0
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+
+    return "c"
+
+def check_board_columns(board, num_rows, num_columns):
+    # check columns
+    for column in range(1, num_columns + 1):
+        count_x = 0
+        count_o = 0
+        for row in range(1, num_rows + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                break
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_o += 1
+                count_x = 0
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+    return "c"
+
 board1 = init_board(6, 7)
 print_board(board1, 6, 7)
-board1 = place_piece(board1, 1, "x",  6, 7)
+board1 = place_piece(board1, 1, "o",  6, 7)
 print_board(board1, 6, 7)
+board1 = place_piece(board1, 2, "x",  6, 7)
+print_board(board1, 6, 7)
+board1 = place_piece(board1, 3, "o",  6, 7)
+print_board(board1, 6, 7)
+status = check_board_rows(board1, 6, 7)
+print(status)
+status = check_board_columns(board1, 6, 7)
+print(status)
+board1 = place_piece(board1, 4, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board_rows(board1, 6, 7)
+print(status)
+status = check_board_columns(board1, 6, 7)
+print(status)
+board1 = place_piece(board1, 5, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board_rows(board1, 6, 7)
+print(status)
+status = check_board_columns(board1, 6, 7)
+print(status)
+board1 = place_piece(board1, 6, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board_rows(board1, 6, 7)
+print(status)
+status = check_board_columns(board1, 6, 7)
+print(status)
+board1 = place_piece(board1, 7, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board_rows(board1, 6, 7)
+print(status)
+status = check_board_columns(board1, 6, 7)
+print(status)
 
