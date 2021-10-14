2912ff97f79779fcb99e7aad390b979d8659441e
doriona <82111572+doriona@users.noreply.github.com>
2021-10-14 19:19:04 +0200

    add input from players and loop to play; fix check for picking column zero

--- connect_four.py
+++ connect_four.py
@@ -23,19 +23,20 @@ def print_board(board, num_rows, num_columns):
     print(("+-" * num_columns) + "+\n" )
 
 def place_piece(board, column, piece, num_rows, num_columns):
-    if column > num_columns:
+    if column > num_columns or column < 1:
         print("Trying to place an {piece} in column {column}".format(
                 piece = piece, column = column))
         print("Make sure to pick a column between 1 and {num_columns}".format(
             num_columns = num_columns))
-        return board
+        return -1, board
     for row in range(1, num_rows + 1):
         pos = str(row) + ',' + str(column)
         if board[pos] == " ":
             board[pos] = piece
-            return board
+            return 0, board
     print("Column {column} is full. Please pick a column that is not full".format(
         column = column))
+    return -1, board
 
 def check_board_rows(board, num_rows, num_columns):
     # check rows
@@ -216,45 +217,89 @@ def check_board(board, num_rows, num_columns):
         return status
     return "c"
 
-board1 = init_board(6, 7)
-print_board(board1, 6, 7)
-board1 = place_piece(board1, 1, "o",  6, 7)
-print_board(board1, 6, 7)
-board1 = place_piece(board1, 2, "x",  6, 7)
-print_board(board1, 6, 7)
-board1 = place_piece(board1, 2, "o",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 3, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 3, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 3, "o",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 4, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 4, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 4, "o",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 4, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 1, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 1 , "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 1, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
-board1 = place_piece(board1, 2, "x",  6, 7)
-print_board(board1, 6, 7)
-status = check_board(board1, 6, 7)
+def get_move(piece, num_columns):
+    message = "Please enter a column number for player {piece} (between 1 and {num_columns}): "
+    column = input(message.format(piece = piece, num_columns = num_columns))
+    return int(column)
+
+def play_game(num_rows, num_columns):
+    board1 = init_board(num_rows, num_columns)
+    print_board(board1, num_rows, num_columns)
+    status = 'c'
+    pieces = ['x','o']
+    piece_index = 0
+    while (status == 'c'):
+        column = get_move(pieces[piece_index], 7)
+        column_error, board1 = place_piece(board1, column, pieces[piece_index], num_rows, num_columns)
+        if column_error == -1:
+            continue
+        print_board(board1, num_rows, num_columns)
+        status = check_board(board1, num_rows, num_columns)
+        if piece_index == 0:
+            piece_index = 1
+        else:
+            piece_index = 0
+    if status == "x":
+        print("Player x won!")
+        return 0
+    elif status == "o":
+        print("Player o won!")
+        return 0
+    elif status == "t":
+        print("It is a tie!")
+        return 0
+    return 1
+
+num_rows = 6
+num_columns = 7
+game_status = 0
+game_init = 'y'
+while game_init != 'n' and game_status == 0:
+    game_init = input("Do you want to play to connect four? [y/n]]: ")
+    if game_init == 'y':
+        game_status = play_game(num_rows, num_columns)
+
+
+# Example game
+#board1 = init_board(6, 7)
+#print_board(board1, 6, 7)
+#board1 = place_piece(board1, 1, "o",  6, 7)
+#print_board(board1, 6, 7)
+#board1 = place_piece(board1, 2, "x",  6, 7)
+#print_board(board1, 6, 7)
+#board1 = place_piece(board1, 2, "o",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 3, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 3, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 3, "o",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 4, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 4, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 4, "o",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 4, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 1, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 1 , "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 1, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
+#board1 = place_piece(board1, 2, "x",  6, 7)
+#print_board(board1, 6, 7)
+#status = check_board(board1, 6, 7)
