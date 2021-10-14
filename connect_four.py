21f96fdd56307c2f8a9969fb61061b12c1c7212e
doriona <82111572+doriona@users.noreply.github.com>
2021-10-14 18:02:09 +0200

    add functions to check diagonals for four pieces

--- connect_four.py
+++ connect_four.py
@@ -83,40 +83,167 @@ def check_board_columns(board, num_rows, num_columns):
                 return "o"
     return "c"
 
+def check_board_diagonals_column(board, num_rows, num_columns):
+    # check diagonals from column
+    for column_main in range(1, num_columns + 1):
+        # down to up
+        count_x = 0
+        count_o = 0
+        row = 1
+        column = column_main
+        while (row < num_rows + 1) and (column < num_columns + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                count_x = 0
+                count_o = 0
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_x = 0
+                count_o += 1
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+            row += 1
+            column += 1
+        # up to down
+        count_x = 0
+        count_o = 0
+        row = num_rows
+        column = column_main
+        while (row >= 1) and (column < num_columns + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                count_x = 0
+                count_o = 0
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_x = 0
+                count_o += 1
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+            row -= 1
+            column += 1
+    return "c"
+
+def check_board_diagonals_row(board, num_rows, num_columns):
+    # check diagonals from rows
+    for row_main in range(1, num_rows + 1):
+        # down to up
+        count_x = 0
+        count_o = 0
+        row = row_main
+        column = 1
+        while (row < num_rows + 1) and (column < num_columns + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                count_x = 0
+                count_o = 0
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_x = 0
+                count_o += 1
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+            row += 1
+            column += 1
+        # up to down
+        count_x = 0
+        count_o = 0
+        row = row_main
+        column = 1
+        while (row >= 1) and (column < num_columns + 1):
+            pos = str(row) + ',' + str(column)
+            if board[pos] == " ":
+                count_x = 0
+                count_o = 0
+            elif board[pos] == "x":
+                count_x += 1
+                count_o = 0
+            elif board[pos] == "o":
+                count_x = 0
+                count_o += 1
+            # check four
+            if count_x == 4:
+                return "x"
+            elif count_o == 4:
+                return "o"
+            row -= 1
+            column += 1
+    return "c"
+
+
+def check_board(board, num_rows, num_columns):
+    status = check_board_rows(board, num_rows, num_columns)
+    print("checking rows: {status}".format(status = status))
+    if status != "c":
+        return status
+    status = check_board_columns(board, num_rows, num_columns)
+    print("checking columns: {status}".format(status = status))
+    if status != "c":
+        return status
+    status = check_board_diagonals_column(board, num_rows, num_columns)
+    print("checking diagonals column: {status}".format(status = status))
+    if status != "c":
+        return status
+    status = check_board_diagonals_row(board, num_rows, num_columns)
+    print("checking diagonals row: {status}".format(status = status))
+    if status != "c":
+        return status
+    return "c"
+
 board1 = init_board(6, 7)
 print_board(board1, 6, 7)
 board1 = place_piece(board1, 1, "o",  6, 7)
 print_board(board1, 6, 7)
 board1 = place_piece(board1, 2, "x",  6, 7)
 print_board(board1, 6, 7)
+board1 = place_piece(board1, 2, "o",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 3, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 3, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
 board1 = place_piece(board1, 3, "o",  6, 7)
 print_board(board1, 6, 7)
-status = check_board_rows(board1, 6, 7)
-print(status)
-status = check_board_columns(board1, 6, 7)
-print(status)
+status = check_board(board1, 6, 7)
 board1 = place_piece(board1, 4, "x",  6, 7)
 print_board(board1, 6, 7)
-status = check_board_rows(board1, 6, 7)
-print(status)
-status = check_board_columns(board1, 6, 7)
-print(status)
-board1 = place_piece(board1, 5, "x",  6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 4, "x",  6, 7)
 print_board(board1, 6, 7)
-status = check_board_rows(board1, 6, 7)
-print(status)
-status = check_board_columns(board1, 6, 7)
-print(status)
-board1 = place_piece(board1, 6, "x",  6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 4, "o",  6, 7)
 print_board(board1, 6, 7)
-status = check_board_rows(board1, 6, 7)
-print(status)
-status = check_board_columns(board1, 6, 7)
-print(status)
-board1 = place_piece(board1, 7, "x",  6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 4, "x",  6, 7)
 print_board(board1, 6, 7)
-status = check_board_rows(board1, 6, 7)
-print(status)
-status = check_board_columns(board1, 6, 7)
-print(status)
-
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 1, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 1 , "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 1, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
+board1 = place_piece(board1, 2, "x",  6, 7)
+print_board(board1, 6, 7)
+status = check_board(board1, 6, 7)
